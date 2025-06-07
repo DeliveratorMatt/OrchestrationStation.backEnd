9e0f834004bd9c518982b67cdfc411113db8c9b9
FredClark <wolfbane6758@gmail.com>
2025-06-07 19:08:32 -0400

    added initial client.js code back into the file, just in case. It is commented out.

--- db/client.js
+++ db/client.js
@@ -1,5 +1,14 @@
 import pg from "pg";
 const db = new pg.Client(process.env.DATABASE_URL);
+// const options = { connectionString: process.env.DATABASE_URL };
+
+// Need SSL for external database connection
+//if (process.env.NODE_ENV === "production") {
+  //options.ssl = { rejectUnauthorized: false };
+}
+
+const db = new pg.Client(options);
+
 export default db;
 // This code initializes a PostgreSQL client using 
 // the pg library and connects it to a database 
