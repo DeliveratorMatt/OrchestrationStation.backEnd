20a978c76abb9683018d5d835312e4fbcd5ea28c
FredClark <wolfbane6758@gmail.com>
2025-06-09 00:46:22 -0400

    Wrote initial seeding code with placeholder values.

--- db/queries/instruments.js
+++ db/queries/instruments.js
@@ -1,10 +1,29 @@
 import db from "#db/client";
 
+// Create a new instrument
+export async function createInstrument({ 
+  name, family, description, imageUrl, range, 
+  famousMusicians, famousExcerpts, 
+  scoreUrl,  history }) {
+  const { rows: [instrument] } = await db.query(
+    `INSERT INTO instruments (name, family, description, imageUrl, range, 
+    famousMusicians, famousExcerpts, 
+    scoreUrl,  history )
+     VALUES ($1, $2, $3, $4, $5, 
+     $6, $7, $8, $9)
+     RETURNING *`,
+    [name, family, description, imageUrl, range, 
+  famousMusicians, famousExcerpts, 
+  scoreUrl,  history ]
+  );
+  return instrument;
+};
+
 // Get all instruments
 export async function getAllInstruments() {
   const { rows } = await db.query(`SELECT * FROM instruments`);
   return rows;
-}
+};
 
 // Get instrument by ID
 export async function getInstrumentById(id) {
@@ -13,7 +32,7 @@ export async function getInstrumentById(id) {
     [id]
   );
   return instrument;
-}
+};
 
 // Search instruments by family
 export async function getInstrumentsByFamily(family) {
